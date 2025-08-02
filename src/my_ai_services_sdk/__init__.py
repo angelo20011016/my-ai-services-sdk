5dadb74d4966821d6a0d36b6df63aee2a9abcc0f
angelo20011016 <angelocandu1@gmail.com>
2025-08-02 16:28:48 +0800

    rename __init__.py

---
+++ src/my_ai_services_sdk/__init__.py
@@ -0,0 +1,12 @@
+# my_ai_services_sdk/my_ai_services_sdk/__init__.py
+from .stt_client import recognize_from_mic
+from .tts_client import speak_text
+# 更新這裡！
+from .gemini_client import generate_content, translate_text 
+
+
+# src/my_ai_services_sdk/__init__.py
+
+from .stt_client import recognize_from_mic
+from .tts_client import speak_text
+from .gemini_client import generate_content, translate_text
